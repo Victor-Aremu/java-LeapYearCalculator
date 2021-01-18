10ca61ccc714843686e773dd2d5092f5e0c71666
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-18 10:36:51 +0000

    computation change

--- src/LeapYearCalculator.java
+++ src/LeapYearCalculator.java
@@ -1,14 +1,8 @@
 public class LeapYearCalculator {
     public static boolean isLeapYear(int year) {
         if (year >= 1 && year <= 9999) {
-            if (year % 4 == 0) {
-                if (year % 100 == 0) {
-                    if (year % 400 == 0) {
-                        return true;
-                    }
-                    return false;
-                }
-                return false;
+            if ((year % 4 ==0)&&(year % 100 ==0)&&(year % 400 ==0)){
+                return true;
             }
         }
         return false;
